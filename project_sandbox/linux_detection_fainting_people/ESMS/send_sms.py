d424c96a9ffee9145a91f67ea22644772802b088
madfalc0n <chadool116@naver.com>
2020-04-22 15:12:22 +0900

    Update project lab
    프로젝트 관련 업데이트
    Emergency SMS API 연동 설정

--- project_sandbox/linux_detection_fainting_people/ESMS/send_sms.py
+++ project_sandbox/linux_detection_fainting_people/ESMS/send_sms.py
@@ -1,5 +1,5 @@
 import requests
-import time 
+import time
 import sys
 import os
 import hashlib
@@ -9,63 +9,76 @@ import json
 from datetime import datetime
 
 """
-네이버 클라우드 플랫폼의 SMS API 이용
+Naver Cloud Platform SMS API
 """
-cur_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-
-timestamp = int(time.time() * 1000)
-timestamp = str(timestamp)
-#print(f"타임스탬프 값: {timestamp}")
+# cur_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+# print(cur_time)
 
 url = "https://sens.apigw.ntruss.com"
 requestUrl = "/sms/v2/services/"
-serviceId = "SMS서비스ID"
+serviceId = "SMS_service_id"
 requestUrl2 = "/messages"
-access_key = "허가된ACCesskey""
-secret_key = "허가된ACCesskey와맵핑되는SecretKey"
+access_key = "access_key"
+secret_key = "secret_key_for_access_key"
 uri = requestUrl + serviceId + requestUrl2
-apiUrl = url+ uri
+apiUrl = url + uri
 
 
-def make_signature(uri,timestamp,access_key,secret_key):
+def make_signature(uri, timestamp, access_key, secret_key):
     secret_key = bytes(secret_key, 'UTF-8')
 
     method = "POST"
-
     message = method + " " + uri + "\n" + timestamp + "\n" + access_key
     message = bytes(message, 'UTF-8')
     signingKey = base64.b64encode(hmac.new(secret_key, message, digestmod=hashlib.sha256).digest())
     return signingKey
 
 
-signature = make_signature(uri,timestamp,access_key,secret_key)
-print(signature)
+def create_data(send_phonenum, recv_phonenum, cur_time):
+    msg = cur_time + " 지하철 객실 내 응급환자 발생 확인 요망"
+    data = {
+        "type": "SMS",
+        "contentType": "COMM",
+        "countryCode": "82",
+        "from": send_phonenum,
+        "content": "message",
+        "messages": [
+            {
+                "to": recv_phonenum,
+                "content": msg
+            }
+        ]
+    }
+    return data
 
-send_phonenum = '010123456'
-recv_phonenum = '010123456'
-msg = cur_time + "지하철 객실 내 응급환자 발생 확인 요망"
 
-data = {
-    "type":"SMS",
-    "contentType":"COMM",
-    "countryCode":"82",
-    "from": send_phonenum,
-    "content":"내용",
-    "messages":[
-        {
-            "to": recv_phonenum,
-            "content": msg
-        }
-    ]
-}
-data2 = json.dumps(data)
-headers = {
-  'Content-Type': 'application/json; charset=utf-8',
-  'X-ncp-apigw-timestamp': timestamp,
-  'x-ncp-iam-access-key': access_key,
-  'x-ncp-apigw-signature-v2': signature
-}
+def send_msg(send_phonenum, recv_phonenum, cur_time):
+    print(f"send : {send_phonenum}, recv : {recv_phonenum}, cur_time : {cur_time}")
+    result = []
 
-response = requests.post(apiUrl, headers=headers, data = data2)
-#결과 확인
-print("Send Message result : ",response.text.encode('utf8'))
+    # 시그니쳐 생성
+    timestamp = int(time.time() * 1000)
+    timestamp = str(timestamp)
+    # print(f"타임스탬프 값: {timestamp}")
+    signature = make_signature(uri, timestamp, access_key, secret_key)
+    print(signature)
+
+    # 헤더 생성
+    headers = {
+        'Content-Type': 'application/json; charset=utf-8',
+        'X-ncp-apigw-timestamp': timestamp,
+        'x-ncp-iam-access-key': access_key,
+        'x-ncp-apigw-signature-v2': signature
+    }
+
+    # SMS 보내기
+    for user in recv_phonenum:
+        data = create_data(send_phonenum, user, cur_time)
+        data2 = json.dumps(data)
+        response = requests.post(apiUrl, headers=headers, data=data2)
+        result.append(response.text.encode('utf8'))
+        time.sleep(0.03)  # 외부와 통신하므로 혹시모르니 딜레이 줌
+
+    # 결과 확인
+    # print("Send Message result : ",result)
+    return result
